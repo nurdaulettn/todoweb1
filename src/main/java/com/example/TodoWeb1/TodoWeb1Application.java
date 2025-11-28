86e59b70a66191e124276795b5ab71e907b1fcb1
nurdaulettn <nurdaulettolegen26@gmail.com>
2025-11-28 14:22:19 +0500

    starting new project Todo list

---
+++ src/main/java/com/example/TodoWeb1/TodoWeb1Application.java
@@ -0,0 +1,13 @@
+package com.example.TodoWeb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoWeb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoWeb1Application.class, args);
+	}
+
+}
